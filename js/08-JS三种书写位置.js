eaae23d2da943aeac0438321b605cd54ed8f4b70
Harlotte <45529303+Harlotte1998@users.noreply.github.com>
2021-11-12 20:34:47 +0800

    Add files via upload

---
+++ js/08-JS三种书写位置.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-17 12:29:23
+ * @LastEditTime: 2021-10-17 12:29:23
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \VSCodeWorkSpace\javaScript零基础通关必备视频\js\08-JS三种书写位置.js
+ */
+
+
+        alert('弹出弹窗')
